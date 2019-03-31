4e1ac7a933da6f997a2f662aa8adcd814e3bb8b4
ShuaiZhang <zhangshuaiisme@gmail.com>
2019-03-31 18:46:11 +0800

    some code

--- 19.namespace/1.test_chroot.c
+++ 19.namespace/1.test_chroot.c
@@ -6,19 +6,20 @@
 
 
 int main(){
+    // 1.修改当前工作目录
+    if(chdir("/home/lzk/Desktop/linux_kernel_security/19.namespace")==-1){
+        perror("chdir");
+        return 0;
+    }
     
-    printf("test chroot\n");
-    
-    
-    // 修改当前进程的root目录
+    // 2.修改当前进程的root目录
     if(chroot("/home/lzk/Desktop/linux_kernel_security/19.namespace")==-1){
         perror("chroot");
         return 0;
     }
     
-    // 执行新的bash进行测试
+    // 3.执行新的bash进行测试, 运行成功，"/"目录被修改为当前目录
     char path[] = "/bin/test_code";
-    printf("[%s]\n",path);
     if(execv(path, NULL) == -1){
         perror("execv");
         return 0;
--- README.md
+++ README.md
@@ -2,7 +2,14 @@
 Some test code of linux kernel security
 
 
-19.namespace：
+#### 19.namespace
 
-因为chroot之后，存在lib依赖的问题，需要提前将
-自己系统的 /lib* 目录拷贝过来
+注意:
+git config --global core.excludesfile ./.gitignore
+
+因为chroot之后，存在lib依赖的问题，需要提前将自己系统的 /lib* 目录拷贝过来
+
+    # 测试程序
+    gcc 1.test_chroot.c
+    sudo ./a.out
+
